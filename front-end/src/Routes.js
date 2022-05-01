591348f63db234cc0c604b8c582386b21c39c450
Oliver <ocben1@gmail.com>
2022-05-01 12:26:12 +1000

    built sign up page

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { LogInPage } from './pages/LogInPage';
 import { UserInfoPage } from './pages/UserInfoPage';
+import { SignUpPage } from './pages/SignUpPage';
 
 export const Routes = () => {
     return (
@@ -11,6 +12,9 @@ export const Routes = () => {
                 </Route>
                 <Route path="/login">
                     <LogInPage />
+                </Route>
+                <Route path="/signup">
+                    <SignUpPage />
                 </Route>
             </Switch>
         </Router>
--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -1,22 +1,23 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export const LogInPage = () => {
+export const SignUpPage = () => {
 
     const [errorMessage, setErrorMessage] = useState('');
 
     const [emailValue, setEmailValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
+    const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
 
     const history = useHistory();
 
-    const onLogInClicked = async () => {
-        alert('Log in not implemented yet');
+    const onSignUpClicked = async () => {
+        alert('Sign up not implemented yet');
     }
 
     return (
         <div className="content-container">
-            <h1>Log In</h1>
+            <h1>Sign Up</h1>
             {errorMessage && <div className="fail">{errorMessage}</div>}
             <input
                 value={emailValue}
@@ -27,12 +28,19 @@ export const LogInPage = () => {
                 value={passwordValue}
                 onChange={e => setPasswordValue(e.target.value)}
                 placeholder="password" />
+            <input
+                type="password"
+                value={confirmPasswordValue}
+                onChange={e => setConfirmPasswordValue(e.target.value)}
+                placeholder="password" />
                 <hr/>
             <button
-                disabled={!emailValue || !passwordValue}
-                onClick={onLogInClicked}>Log In</button>
-            <button onClick={() => history.push('/forgot-password')}>Forgot your password?</button>
-            <button onClick={() => history.push('/signup')}>Don't have an account? Sign up</button>
+                disabled={
+                    !emailValue || !passwordValue ||
+                            passwordValue !== confirmPasswordValue
+                }
+                onClick={onSignUpClicked}>Sign Up</button>
+            <button onClick={() => history.push('/login')}>Already have an account? Log In</button>
         </div>
     );
 }
