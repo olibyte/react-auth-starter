e10792ccb189692b23971f388cc9c622dedfb5b4
Oliver <ocben1@gmail.com>
2022-05-15 07:13:38 +1000

    implemented jwts on the front end

--- front-end/src/auth/useToken.js
+++ front-end/src/auth/useToken.js
@@ -1,5 +1,14 @@
 import { useState } from "react";
 
 export const useToken = () => {
-    
+    const [token, setTokenInternal] = useState(() => {
+        return localStorage.getItem('token');
+    });
+
+    const setToken = newToken => {
+        localStorage.setItem('token', newToken);
+        setTokenInternal(newToken);
+    }
+
+    return [token, setToken];
 }
