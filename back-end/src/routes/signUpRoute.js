e5025cd4fd6cd7acecc519b264ff7b0b96b0e859
Oliver <ocben1@gmail.com>
2022-05-21 08:28:30 +1000

    integrated verification tokens

--- back-end/src/routes/signUpRoute.js
+++ back-end/src/routes/signUpRoute.js
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { v4 as uuid } from 'uuid';
 import { getDbConnection } from '../db';
+import { sendEmail } from '../util/sendEmail';
 
 export const signUpRoute = {
     path: '/api/signup',
@@ -17,6 +19,8 @@ export const signUpRoute = {
 
         const passwordHash = await bcrypt.hash(password, 10);
 
+        const verificationString = uuid();
+
         const startingInfo = {
             hairColor: '',
             favoriteFood: '',
@@ -28,8 +32,24 @@ export const signUpRoute = {
             passwordHash,
             info: startingInfo,
             isVerified: false,
+            verificationString,
         });
         const { insertedId } = result;
+
+        try {
+            await sendEmail({
+                to: email,
+                from: process.env.MY_EMAIL,
+                subject: 'Please verify your email',
+                text: `
+                    Thanks for signing up! To verify your email, click here:
+                    http://localhost:3000/verify-email/${verificationString}
+                `,
+            });
+        } catch (e) {
+            console.log(e);
+            res.sendStatus(500);
+        }
 
         jwt.sign({
             id: insertedId,
--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -28,9 +28,10 @@ export const updateUserInfoRoute = {
         jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
             if (err) return res.status(401).json({ message: 'Unable to verify token' });
 
-            const { id } = decoded;
+            const { id, isVerified } = decoded;
 
             if (id !== userId) return res.status(403).json({ message: 'Not allowed to update that user\'s data' });
+            if (!isVerified) return res.status(403).json({ message: 'You need to verify your email before you can update your data'})
 
             const db = getDbConnection('react-auth-db');
             const result = await db.collection('users').findOneAndUpdate(
@@ -38,7 +39,7 @@ export const updateUserInfoRoute = {
                 { $set: { info: updates } },
                 { returnOriginal: false },
             );
-            const { email, isVerified, info } = result.value;
+            const { email, info } = result.value;
 
             jwt.sign({ id, email, isVerified, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
                 if (err) {
