cd98357cbb0f16d279498f42688bb6d01743cc0a
Oliver <ocben1@gmail.com>
2022-05-21 09:45:32 +1000

    add verify email route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,10 +2,11 @@ import { logInRoute } from './logInRoute';
 import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
-
+import { verifyEmailRoute } from './verifyEmailRoute';
 export const routes = [
     logInRoute,
     signUpRoute,
     testRoute,
     updateUserInfoRoute,
+    verifyEmailRoute,
 ];
