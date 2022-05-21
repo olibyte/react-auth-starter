e5779aeab512171bd59a4b38e50f6d31d39de55b
Oliver <ocben1@gmail.com>
2022-05-22 09:15:03 +1000

    fixed token undefined issue after email verification

--- back-end/src/routes/verifyEmailRoute.js
+++ back-end/src/routes/verifyEmailRoute.js
@@ -5,7 +5,7 @@ import { getDbConnection } from '../db';
 export const verifyEmailRoute = {
     path: '/api/verify-email',
     method: 'put',
-    handler: async (req,res) => {
+    handler: async (req, res) => {
         const { verificationString } = req.body;
         const db = getDbConnection('react-auth-db');
         const result = await db.collection('users').findOne({
@@ -14,15 +14,15 @@ export const verifyEmailRoute = {
 
         if (!result) return res.status(401).json({ message: 'The email verification code is incorrect' });
 
-        const { _id: id, email, info} = result;
+        const { _id: id, email, info } = result;
 
         await db.collection('users').updateOne({ _id: ObjectID(id) }, {
             $set: { isVerified: true }
         });
 
-        jwt.sign({ id, email, isVerified: true, info }, process.env.JWT_SECRET, { expiresIn: '2d'}, (err, token) => {
+        jwt.sign({ id, email, isVerified: true, info }, process.env.JWT_SECRET, { expiresIn: '2d' }, (err, token) => {
             if (err) return res.sendStatus(500);
-            res.sendStatus(200).json({ token });
+            res.status(200).json({ token });
         });
     }
 }
