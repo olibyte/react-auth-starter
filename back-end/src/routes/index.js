aab6c1abfb7060a259b605b93c3b55881a624234
Oliver <ocben1@gmail.com>
2022-05-23 07:20:32 +1000

    send reset password email functionality

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,9 +1,11 @@
+import { forgotPasswordRoute } from './forgotPasswordRoute';
 import { logInRoute } from './logInRoute';
 import { signUpRoute } from './signUpRoute';
 import { testRoute } from './testRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
 import { verifyEmailRoute } from './verifyEmailRoute';
 export const routes = [
+    forgotPasswordRoute,
     logInRoute,
     signUpRoute,
     testRoute,
