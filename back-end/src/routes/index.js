4e29c5c1f6945592202c282262e9ca24689f82e8
Oliver <ocben1@gmail.com>
2022-07-17 10:03:28 +1000

    add oauth url route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,4 +1,5 @@
 import { forgotPasswordRoute } from './forgotPasswordRoute';
+import { getGoogleOauthUrlRoute } from './getGoogleOauthUrlRoute';
 import { logInRoute } from './logInRoute';
 import { resetPasswordRoute } from './resetPasswordRoute';
 import { signUpRoute } from './signUpRoute';
@@ -7,6 +8,7 @@ import { updateUserInfoRoute } from './updateUserInfoRoute';
 import { verifyEmailRoute } from './verifyEmailRoute';
 export const routes = [
     forgotPasswordRoute,
+    getGoogleOauthUrlRoute,
     logInRoute,
     resetPasswordRoute,
     signUpRoute,
